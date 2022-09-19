5fd518124ee804af689b6b0e797291dc70a7882d
Kenan <kenan23@gmail.com>
2022-09-19 17:07:42 +0300

    Add hasura SQL engine

--- app/connectivity/models.py
+++ app/connectivity/models.py
@@ -1,12 +1,23 @@
 from django.db import models
+from django.db.models import Count, F, Value
+
+import datetime
+
+
+class ConnectivityManager(models.Manager):
+    def update_counter(self, pk):
+        self.filter(id=pk).update(count=F('count') + 1)
 
 
 class Connectivity(models.Model):
-    IP = models.GenericIPAddressField()
+    ip = models.GenericIPAddressField(unique=True)
     description = models.CharField(max_length=255)
     is_accessible = models.BooleanField(default=False)
     is_enabled = models.BooleanField(default=False)
-    update_date = models.DateTimeField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(default=datetime.datetime.now())
+    update_counter = models.PositiveIntegerField(default=0)
+    objects = ConnectivityManager()
 
     def __str__(self):
         return self.IP
--- app/connectivity/schema.py
+++ app/connectivity/schema.py
@@ -1,4 +1,5 @@
 import graphene
+from graphene import ObjectType
 from graphene_django import DjangoObjectType
 from .models import Connectivity
 
@@ -6,17 +7,23 @@ from .models import Connectivity
 class ConnectivityType(DjangoObjectType):
     class Meta:
         model = Connectivity
-        fields = ("id", "IP", "description",
-                  "is_accessible", "is_enabled",
-                  "update_date")
+        fields = ("id",
+                  "ip",
+                  "description",
+                  "is_accessible",
+                  "is_enabled",
+                  "update_date",)
 
 
 class Query(graphene.ObjectType):
-    all_connectees = graphene.List(ConnectivityType)
-    def resolve_all_connectees(root, info):
-        # return Connectivity.objects.all()
-        return Connectivity.objects.filter(id='1')
+    all_connections = graphene.List(ConnectivityType)
+    connnection_by_ip = graphene.Field(ConnectivityType, ip=graphene.String())
 
+    def resolve_all_connections(root, info):
+        return Connectivity.objects.all()
+
+    def resolve_connnection_by_ip(root, info, ip):
+        return Connectivity.objects.get(IP=ip)[:0]
 
 
 schema = graphene.Schema(query=Query)
